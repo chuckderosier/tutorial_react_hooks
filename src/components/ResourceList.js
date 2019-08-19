2f487f2712c33c51a1ce84c38a5a8255d2ce6e4d
chuckderosier <chuckderosier2018@gmail.com>
2019-08-19 16:09:46 -0400

    break in tutorial

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,34 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-class ResourceList extends React.Component {
-  state = {
-    resources: [],
-  }
+const ResourceList = () => {
+  const [ resources, setResources ] = useState([]);
 
-  async componentDidMount() {   // loads initial data posts with lifecycle methods
+  const setResources = async () => {      // async because of await one way call
     const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resourceName}`);
 
     this.setState({ resources: response.data });
   }
 
-  async componentDidUpdate(prevProps) {   // switches data when button clicked with lifecycle methods
-    if(prevProps.resourceName !== this.props.resourceName) {
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resourceName}`);
-
-      this.setState({ resources: response.data });
-    }
-  }
-
-  render() {
-    return (
-      <ul>
-        {this.state.resources.map(resource => (
-          <li key={resource.id}>{resource.title}</li>
-        ))}
-      </ul>
-    )
-  }
+  return (
+    // <ul>
+    //   {this.state.resources.map(resource => (
+    //     <li key={resource.id}>{resource.title}</li>
+    //   ))}
+    // </ul>
+  )
 }
+
+// class ResourceList extends React.Component {
+//   state = {
+//     resources: [],
+//   }
+
+//   async componentDidMount() {   // loads initial data posts with lifecycle methods
+//     const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resourceName}`);
+
+//     this.setState({ resources: response.data });
+//   }
+
+//   async componentDidUpdate(prevProps) {   // switches data when button clicked with lifecycle methods
+//     if(prevProps.resourceName !== this.props.resourceName) {
+//       const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resourceName}`);
+
+//       this.setState({ resources: response.data });
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <ul>
+//         {this.state.resources.map(resource => (
+//           <li key={resource.id}>{resource.title}</li>
+//         ))}
+//       </ul>
+//     )
+//   }
+// }
 
 export default ResourceList;
